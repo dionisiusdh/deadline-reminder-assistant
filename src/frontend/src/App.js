65cac0adbe8f1ccce14b21fd6f7738b6a1ae9094
16519111 <gregdimasbaskara@gmail.com>
2021-04-28 13:00:14 +0700

    Frontend: Add Gender Feature

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,17 +3,53 @@ import ChatBox from "./components/chat/ChatBox";
 import "./App.css";
 
 function App() {
+  const [gender, setGender] = React.useState(0)
   return (
     <>
       <div className="container d-flex flex-column align-items-center justify-content-center">
         <div className="home-title">
-          <div className="home-title-content d-flex flex-column align-items-start">
-            <h1>GhemBOT</h1>
-            <h2>Deadline Reminder Assistant Chatbot</h2>
+          <div className="d-flex flex-row justify-content-between">
+            <div className="home-title-content d-flex flex-column align-items-start">
+              <h1>GhemBOT</h1>
+              <h2>Deadline Reminder Assistant Chatbot</h2>
+            </div>
+            <div className="d-flex flex-row align-items-center">
+              {gender == 0 ?
+                  <>
+                    <img
+                      src="https://previews.123rf.com/images/victor85/victor851711/victor85171100224/90388679-male-sex-symbol-circle-icon-black-round-minimalist-icon-isolated-on-white-background-gender-symbol-s.jpg"
+                      alt="pic"
+                      onClick={() => setGender(0)}
+                      className="img-selected"
+                    />
+                    <img
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7FKv6jQfsogh9NRdn-wfRdYhIVxz5amLlmw&usqp=CAU"
+                      alt="pic"
+                      onClick={() => setGender(1)}
+                      className="img-unselected"
+                    />
+                  </>
+                :
+                  <>
+                    <img
+                      src="https://previews.123rf.com/images/victor85/victor851711/victor85171100224/90388679-male-sex-symbol-circle-icon-black-round-minimalist-icon-isolated-on-white-background-gender-symbol-s.jpg"
+                      alt="pic"
+                      onClick={() => setGender(0)}
+                      className="img-unselected"
+                    />
+                    <img
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7FKv6jQfsogh9NRdn-wfRdYhIVxz5amLlmw&usqp=CAU"
+                      alt="pic"
+                      onClick={() => setGender(1)}
+                      className="img-selected"
+                    />
+                  </>
+              }
+            </div>
           </div>
         </div>
 
-        <ChatBox />
+        <ChatBox gender={gender}/>
       </div>
       <style>
         {`
@@ -32,6 +68,18 @@ function App() {
           .home-title-content h2 {
             font-size: 1rem;
             font-weight: 200;
+          }
+
+          .img-selected:hover {
+            cursor: pointer;
+          }
+
+          .img-unselected {
+            opacity: 0.5;
+          }
+
+          .img-unselected:hover {
+            cursor: pointer;
           }
         `}
       </style>
